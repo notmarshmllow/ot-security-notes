58c4e0aa9409f36809a0ce70d8af735694519c2a
notmarshmllow <60079743+notmarshmllow@users.noreply.github.com>
2024-03-24 21:25:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
-github.com/avaoeoeo/danduada
-"github.com/wdadbiadbiwdbuyawbdabd/wertydfg"
-fmt
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	template2 "html/template"
+	"net/url"
+	"os"
+	"path/filepath"
+	"text/template"
 
-aniue
-github.com
-github.com/rtyfghcv/tyghbcswghj/tuybib
+	"github.com/avelino/awesome-go/pkg/markdown"
+	cp "github.com/otiai10/copy"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/avelino/awesome-go/pkg/slug"
+)
+
+// Link contains info about awesome url
+type Link struct {
+	Title       string
+	URL         string
+	Description string
+}
